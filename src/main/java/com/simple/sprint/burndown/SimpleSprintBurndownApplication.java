5976bfc5e400e69388f3804da72b6ae3b68ec6c7
matheusaugusto1994 <m.augusto1994@gmail.com>
2016-08-15 16:07:38 -0300

    rename

--- src/main/java/com/simple/sprint/burndown/SimpleSprintBurndownApplication.java
+++ src/main/java/com/simple/sprint/burndown/SimpleSprintBurndownApplication.java
@@ -1,12 +1,12 @@
-package com.sample.sprint.burndown;
+package com.simple.sprint.burndown;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SampleSprintBurndownApplication {
+public class SimpleSprintBurndownApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SampleSprintBurndownApplication.class, args);
+		SpringApplication.run(SimpleSprintBurndownApplication.class, args);
 	}
 }
