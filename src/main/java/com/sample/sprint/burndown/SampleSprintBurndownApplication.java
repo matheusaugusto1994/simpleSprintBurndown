fd460134241100b0f816f4a38cad2e2b9e508898
matheusaugusto1994 <m.augusto1994@gmail.com>
2016-08-15 15:51:53 -0300

    first commit

---
+++ src/main/java/com/sample/sprint/burndown/SampleSprintBurndownApplication.java
@@ -0,0 +1,12 @@
+package com.sample.sprint.burndown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSprintBurndownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSprintBurndownApplication.class, args);
+	}
+}
